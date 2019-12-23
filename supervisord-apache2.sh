967d55e2ea1ba16f4a269a4dcaa89b2da2b672a9
Daniel Binggeli <drnoa@users.noreply.github.com>
2019-12-23 22:19:23 +0100

    Change PHP Ini Path

--- supervisord-apache2.sh
+++ supervisord-apache2.sh
@@ -14,24 +14,25 @@ fi
 # Configuring PHP open_short_tag
 OPEN_SHORT_TAG=${SHORT_TAG:-false}
 if [ "$OPEN_SHORT_TAG" = true ] ; then
-    sed -i -r 's/short_open_tag = Off$/short_open_tag = On/' /etc/php5/apache2/php.ini
-    sed -i -r 's/short_open_tag = Off$/short_open_tag = On/' /etc/cli/apache2/php.ini
+    sed -i -r 's/short_open_tag = Off$/short_open_tag = On/' /etc/php/7.2/apache2/php.ini
+    sed -i -r 's/short_open_tag = Off$/short_open_tag = On/' /etc/php/7.2/cli/php.ini
 fi
 if [ "$OPEN_SHORT_TAG" = false ] ; then
-    sed -i -r 's/short_open_tag = On$/short_open_tag = Off/' /etc/php5/apache2/php.ini
-    sed -i -r 's/short_open_tag = On$/short_open_tag = Off/' /etc/cli/apache2/php.ini
+    sed -i -r 's/short_open_tag = On$/short_open_tag = Off/' /etc/php/7.2/apache2/php.ini
+    sed -i -r 's/short_open_tag = On$/short_open_tag = Off/' /etc/php/7.2/cli/php.ini
 fi
 
 # Configuring PHP display_errors
 DISPLAY_ERRORS=${ERRORS:-false}
 if [ "$DISPLAY_ERRORS" = true ] ; then
-    sed -i -r 's/display_errors = Off$/display_errors = On/' /etc/php5/apache2/php.ini
-    sed -i -r 's/display_errors = Off$/display_errors = On/' /etc/cli/apache2/php.ini
+    sed -i -r 's/display_errors = Off$/display_errors = On/' /etc/php/7.2/apache2/php.ini
+    sed -i -r 's/display_errors = Off$/display_errors = On/' /etc/php/7.2/cli/php.ini
 fi
 if [ "$DISPLAY_ERRORS" = false ] ; then
-    sed -i -r 's/display_errors = On$/display_errors = Off/' /etc/php5/apache2/php.ini
-    sed -i -r 's/display_errors = On$/display_errors = Off/' /etc/cli/apache2/php.ini
+    sed -i -r 's/display_errors = On$/display_errors = Off/' /etc/php/7.2/apache2/php.ini
+    sed -i -r 's/display_errors = On$/display_errors = Off/' /etc/php/7.2/cli/php.ini
 fi
 # Starting the apache2 server
+a2enmod fcgid
 source /etc/apache2/envvars
 exec apache2 -D FOREGROUND
